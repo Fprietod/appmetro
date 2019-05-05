01173e54539dd0f58d37f4d57620859656d5f14f
F.Prieto <felipe00@live.com.mx>
2019-05-05 03:29:30 -0500

    Corregido

--- app/src/main/java/com/example/felipe/metro2/Main3Activity.java
+++ app/src/main/java/com/example/felipe/metro2/Main3Activity.java
@@ -1,13 +1,24 @@
 package com.example.felipe.metro2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main3Activity extends AppCompatActivity {
-
+    Button confirmarreporte;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        confirmarreporte = (Button) findViewById(R.id.idReportar);
+        confirmarreporte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Main3Activity.this, Main4Activity.class);
+                startActivity(i);
+            }
+        });
     }
 }
