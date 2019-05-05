3336f4258063267734ecbaba6eeb99c652531ff8
F.Prieto <felipe00@live.com.mx>
2019-05-05 02:37:23 -0500

    Ya uni una activity

--- app/src/main/java/com/example/felipe/metro2/MainActivity.java
+++ app/src/main/java/com/example/felipe/metro2/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.felipe.metro2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button botonlinea;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        botonlinea = (Button) findViewById(R.id.idbtnLineas);
+        botonlinea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(i);
+            }
+        });
     }
 }
