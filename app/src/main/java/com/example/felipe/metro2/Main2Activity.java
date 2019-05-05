3df1583c89fff82b5d494444ee8596f451719c10
F.Prieto <felipe00@live.com.mx>
2019-05-05 02:49:25 -0500

    Agregado una nueva actividad

--- app/src/main/java/com/example/felipe/metro2/Main2Activity.java
+++ app/src/main/java/com/example/felipe/metro2/Main2Activity.java
@@ -1,20 +1,30 @@
 package com.example.felipe.metro2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
-
+    Button btnregresar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        btnregresar = (Button) findViewById(R.id.idbtnRegresarInicio);
+        btnregresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Main2Activity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
